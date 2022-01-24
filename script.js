b4fbf627c286d362bf96db968105264aefd9161b
apcaacabanas <97135911+aacabanas@users.noreply.github.com>
2022-01-24 09:18:24 +0800

    Added js and css

---
+++ script.js
@@ -0,0 +1,40 @@
+function Name(){
+    const name = document.getElementById("name")
+    if(name.innerHTML=="[REMOVED]"){
+        name.innerHTML="Alexander Cabanas"
+    }else{
+        name.innerHTML="[REMOVED]"
+    }
+}
+function Age(){
+    const name = document.getElementById("age")
+    if(name.innerHTML=="[REMOVED]"){
+        name.innerHTML="21"
+    }else{
+        name.innerHTML="[REMOVED]"
+    }
+}
+function Nick(){
+    const name = document.getElementById("nick")
+    if(name.innerHTML=="[REMOVED]"){
+        name.innerHTML="Von"
+    }else{
+        name.innerHTML="[REMOVED]"
+    }
+}
+function BDate(){
+    const name = document.getElementById("bdate")
+    if(name.innerHTML=="[REMOVED]"){
+        name.innerHTML="11/30/2000"
+    }else{
+        name.innerHTML="[REMOVED]"
+    }
+}
+function HSchool(){
+    const name = document.getElementById("hschool")
+    if(name.innerHTML=="[REMOVED]"){
+        name.innerHTML="Junior High: South Crest School Senior High: Asia Pacific College"
+    }else{
+        name.innerHTML="[REMOVED]"
+    }
+}
